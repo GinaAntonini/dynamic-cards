eb2675804e4180ac43db84be4149f4aa21df18d5
Gina Antonini <ginamantonini@gmail.com>
2017-08-17 20:37:53 -0500

    added more functions

--- cards.js
+++ cards.js
@@ -1,37 +1,35 @@
 
 // When the user enters in text into the text area and then clicks the create button, create a new card element in the DOM that includes it's own delete button. You decide the height/width of the card.
 
-var cards = ["asfgdnsgs", "ahsfnsiaofn", "asfafsgasg"];
+var cards = [];
 
 var cardContainerDiv= document.getElementById('cardContainer');
 var inputField= document.getElementById('textarea');
-var createNewCard= document.getElementById('createButton');
+var createButton= document.getElementById('createButton');
 
-function createNewCard (){
-	var newCard = 
+function createNewCard (arr){
+	arr.push(inputField.value)
 }
-
 
 function buildDomString (arr){
 	var cardString = '';
 	for(var i = 0; i < arr.length; i++){
 		var domString = "";
-	domString+=`<div class="cardContainer" id="cardContainer">`;
+	domString+=`<div class="card" id="card">`;
+	domString+=`<p>${cards[i]}</p>`;
     domString+=`<button id="deleteButton">Delete</button>`;
     domString+=`</div>`;
     cardString += domString;
 	}
+	cardContainerDiv.innerHTML = cardString;
 	console.log(cardString);
 };
 
-function writeToDom(strang){
-	cardContainer.innerHTML = strang;
-}
-
-createNewCard.addEventListener('click',function(){
-	buildDomString();
+createButton.addEventListener('click',function(){
+	createNewCard(cards);
+	buildDomString(cards);
+	console.log(cards);
 });
 
 // When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
 
-buildDomString(cards);
